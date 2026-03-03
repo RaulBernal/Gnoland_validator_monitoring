ed189f550786b2f89f010fb9ed20718f5872cb0d
Raul Bernal <bernal.raul@gmail.com>
2026-03-03 23:56:49 +0100

    fix Telegram name in example

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ TELEGRAM_CHAT_ID   = "-1001234567890"   # use negative ID for groups/channels
 # address monitoring variables
 validators_to_mon_gnoland = [
     {"name": 'Validator 1',      "validator": 'g1zmua93u53na9xsvtmlh5d8p7pm0w7p52ehewc9',  "telegram": '@Raul_CTO_AviaOne'},
-    {"name": 'Validator AviaOne',"validator": 'g1e5sxezpafa8lcv5xu3nmw4plz30mnepq2wv9xs',  "telegram": '@Raul\_CTO\_AviaOne @Aviaone'},
+    {"name": 'Validator AviaOne',"validator": 'g1e5sxezpafa8lcv5xu3nmw4plz30mnepq2wv9xs',  "telegram": '@Raul_CTO_AviaOne @Aviaone'},
     {"name": 'Validator 2',      "validator": 'g1k9cp2fz6n3fa4zftx7cz6wv2hwzaz6lkr0xk7z', "telegram": '@Raul_CTO_AviaOne'},
 ]
